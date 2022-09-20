571cfb64b2c49bd7380ee1f90d68e7a9981a453b
gabito1451 <110038450+gabito1451@users.noreply.github.com>
2022-09-20 20:58:25 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+//const hamburger_icon = document.querySelector(
+"#nav-bar .nav-bar .nav-list .hamburger";
+//);
+//const nav_list = document.querySelector("#nav-bar .nav-bar .nav-list");
+//hamburger_icon.addEventListener("click", () => {
+//nav_list.classList.toggle("open");
+//hamburger_icon.classList.toggle("active");
+//});
+
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
